ca59102535debd5e8eaf27c05a51b1ef2927ffb9
saify96 <mohammad.alsaify@gmail.com>
2021-08-03 13:29:52 +0300

    challenge07

--- challenge05/lib/src/main/java/challenge05/Main.java
+++ challenge05/lib/src/main/java/challenge05/Main.java
@@ -2,14 +2,15 @@ package challenge05;
 
 public class Main {
     public static void main(String[] args) {
-//        LinkedList testLL = new LinkedList();
-//        testLL.insert(1);
-//        testLL.append(2);
-//        testLL.append(3);
-//        testLL.append(4);
-//        System.out.println(testLL.kthFromEnd(10));
-        LinkedList firstLinkedList = new LinkedList();
-        firstLinkedList.insert(5);
-        firstLinkedList.toString();
+        LinkedList testLL = new LinkedList();
+        testLL.insert(1);
+        testLL.append(2);
+        testLL.append(3);
+        testLL.append(4);
+
+        System.out.println(testLL.kthFromEnd(10));
+//        LinkedList firstLinkedList = new LinkedList();
+//        firstLinkedList.insert(5);
+//        firstLinkedList.toString();
     }
 }
