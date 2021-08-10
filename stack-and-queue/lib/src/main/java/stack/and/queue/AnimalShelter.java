e207f16f0e018e318198a3181278ff1730158970
saify96 <mohammad.alsaify@gmail.com>
2021-08-10 17:46:18 +0300

    complete functions codechallenge 12

--- stack-and-queue/lib/src/main/java/stack/and/queue/AnimalShelter.java
+++ stack-and-queue/lib/src/main/java/stack/and/queue/AnimalShelter.java
@@ -1,17 +1,80 @@
 package stack.and.queue;
 
-public class AnimalShelter {
-    Queue<AnimalShelter> animalQueue = new Queue<>();
+public class AnimalShelter<T> {
+    Queue<Animal> animalQueue = new Queue<>();
 
-    public void animalEnqueue(AnimalShelter animal) {
+    public void animalEnqueue(Animal animal) {
         animalQueue.Enqueue(animal);
     }
 
     public void animalDequeue(String type) {
-        while(animalQueue.front.toString()!=type){
-            animalQueue.dequeue();
+        Queue<Animal> newAnimalQueue = new Queue<>();
+        while (!animalQueue.front.value.getClass().getSimpleName().toLowerCase().equals(type) && animalQueue.front != null) {
+            newAnimalQueue.Enqueue(animalQueue.dequeue());
         }
         animalQueue.dequeue();
+        while (animalQueue.front != null) {
+            newAnimalQueue.Enqueue(animalQueue.dequeue());
+        }
+        animalQueue.front = newAnimalQueue.front;
+    }
+
+    @Override
+    public String toString() {
+        Node current = animalQueue.front;
+        String queue = "";
+        queue = "FRONT -> ";
+        while (current != null) {
+            queue += "{" + current.value + "} -> ";
+            current = current.next;
+        }
+        queue += "NULL";
+        return queue;
     }
 
 }
+
+
+//
+//package stack.and.queue;
+//
+//        import java.util.Locale;
+//
+//public class AnimalShelter<T> {
+//    Queue<T> animalQueue = new Queue<>();
+//
+//    public void animalEnqueue(T animal) {
+//        animalQueue.Enqueue(animal);
+//    }
+//
+//    public T animalDequeue(String type) {
+//        Queue<T> newAnimalQueue = new Queue<>();
+////        if (type=="cat")
+////            Cat catIns =new Cat();
+//        while(animalQueue.front.value.toString()!=type && animalQueue.front!=null){
+////        while(animalQueue.front.value.getClass().getSimpleName().toLowerCase()!=type && animalQueue.front!=null){
+//            newAnimalQueue.Enqueue(animalQueue.dequeue());
+//        }
+//        T animal = animalQueue.dequeue();
+//        while(animalQueue.front!=null){
+//            newAnimalQueue.Enqueue(animalQueue.dequeue());
+//        }
+//        animalQueue.front=newAnimalQueue.front;
+//        return animal;
+//
+//    }
+//
+//    @Override
+//    public String toString() {
+//        Node current = animalQueue.front;
+//        String queue = "";
+//        queue ="FRONT -> ";
+//        while (current != null) {
+//            queue += "{" + current.value+ "} -> ";
+//            current = current.next;
+//        }
+//        queue += "NULL";
+//        return queue;
+//    }
+//
+//}
