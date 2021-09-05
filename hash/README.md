c1c330dd162978481be8ebd0cbcdb7782d7d8266
saify96 <mohammad.alsaify@gmail.com>
2021-09-06 00:39:02 +0300

    hashTable implementation

---
+++ hash/README.md
@@ -0,0 +1,7 @@
+# Hashtables
+Hashtables are a data structure that utilize key value pairs. This means every Node or Bucket has both a key, and a value.
+## Challenge
+Write the implementation for HashTable with add, get, contains and hash methods.
+## Approach & Efficiency
+time O(1)
+space O(n)
--- hash/lib/src/main/java/hash/HashTable.java
+++ hash/lib/src/main/java/hash/HashTable.java
@@ -1,28 +1,70 @@
 package hash;
 
-public class HashTable<T> {
-    int size;
-    Node[] tableArray;
+import java.util.NoSuchElementException;
+
+public class HashTable<K, T> {
+    Node<K, T>[] tableArray;
 
     public HashTable(int size) {
-        this.size = size;
-        tableArray = new Node[this.size];
-        for (int i = 0; i < tableArray.length; i++) {
-            tableArray[i] = new Node();
-        }
+        tableArray = new Node[size];
     }
 
-    public int GetHash(String key) {
+    public int GetHash(K key) {
         int sumOfAscii = 0;
-        for (int i = 0; i < key.length(); i++) {
-            sumOfAscii += (int) key.charAt(i);
-            System.out.println((int) key.charAt(i));
+        for (int i = 0; i < key.toString().length(); i++) {
+            sumOfAscii += (int) key.toString().charAt(i);
+//            System.out.println((int) key.charAt(i));
         }
         return (sumOfAscii * 599) % tableArray.length;
     }
 
-    public void addToHashTable(String key, T value) {
-        int hashedKey = GetHash(key);
-        Node newItem = new Node(hashedKey, value);
+    public void addToHashTable(K key, T value) {
+        int hashedKeyIdx = GetHash(key);
+        if (tableArray[hashedKeyIdx] == null) {
+            tableArray[hashedKeyIdx] = new Node<>(key, value);
+        } else {
+            if (key == tableArray[hashedKeyIdx].getKey()) {
+                tableArray[hashedKeyIdx].setValue(value);
+            } else {
+                Node<K, T> newItem = new Node(key, value);
+                tableArray[hashedKeyIdx].next = newItem;
+            }
+        }
+    }
+
+    public T get(K key) {
+        int hashedKeyIdx = GetHash(key);
+        if (tableArray[hashedKeyIdx] != null) {
+            if (key == tableArray[hashedKeyIdx].getKey()) {
+                return tableArray[hashedKeyIdx].getValue();
+            } else {
+                Node<K, T> current = tableArray[hashedKeyIdx];
+                while (current.next != null) {
+                    if (current.next.getKey() == key) {
+                        return (T) current.next.value;
+                    }
+                    current = current.next;
+                }
+            }
+        }
+        throw new NoSuchElementException("There is no element with such Key");
+    }
+
+    public boolean contain(K key) {
+        int hashedKeyIdx = GetHash(key);
+        if (tableArray[hashedKeyIdx] != null) {
+            if (tableArray[hashedKeyIdx].getKey() == key) {
+                return true;
+            } else {
+                Node<K, T> current = tableArray[hashedKeyIdx];
+                while (current.next != null) {
+                    if (current.next.getKey() == key) {
+                        return true;
+                    }
+                    current = current.next;
+                }
+            }
+        }
+        return false;
     }
 }
